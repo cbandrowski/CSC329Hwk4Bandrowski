3a386663abf4e6e9edcb05e5425b4802910f2df7
“Cody <cbandrowski@gmail.com//>
2025-04-27 11:04:40 -0400

    part 6 done

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 /**
  * MyGraph class
- * Using Edge class to make and keep track of your graph and
+ * Using Edge class to make and keep track of your graph and its components
  * @author  Cody Bandrowski
  */
 public class MyGraph {
@@ -89,7 +89,7 @@ public class MyGraph {
             System.out.println();
         }
     }
-
+    //Part 4 Connected Components -------------------------------------------------------
     /**
      * calculateConnectedComponents
      * used to find number of components in Graph
@@ -126,6 +126,7 @@ public class MyGraph {
          }
         return connectedComponents;
     }
+    //Part 5 Minimum Spanning tree-------------------------------------------------------
     /**
      * getMinFrontierEdge
      * gets min-weight of edge in Graph
@@ -200,6 +201,89 @@ public class MyGraph {
         //return MST
         return mst;
     }
+    // Part 6 - Shortest Path -----------------------------------------------------------------------------
 
-}
+    /**
+     *getMinDistVertex
+     * Searches list of unvisited vertices for one with minimum distance
+     * @param g         the graph
+     * @param unvisitedList     list of vertices not yet processed
+     * @param dist      array where dist[v] is shortest distance
+     * @return          the vertex in unvisitedList with smallest dist
+     */
+    public static int getMinDistVertex(MyGraph g, List<Integer> unvisitedList, int[] dist){
+        //minNeighborVertex = unvisitedList first element
+        int minNeighborVertex = unvisitedList.get(0);
+        //minNeighborDist = unvisitedList first element dist
+        int minNeighborDist = dist[minNeighborVertex];
+
+        //For all vertex v in unvisitedList
+        for(int v : unvisitedList) {
+            //if dist[v] < minNeighborDist
+            if (dist[v] < minNeighborDist) {
+                //minNeighborDist = dist[v]
+                minNeighborDist = dist[v];
+                //minNeighborVertex = v
+                minNeighborVertex = v;
+            }
+        }
+        //End for
+        //return minNeighborVertex
+        return minNeighborVertex;
+    }
+
+    /**
+     *shortestPath
+     * using getMinDistVertex it finds the shortest path
+     * @param g
+     * @param startingVertex
+     */
+    public static void shortestPath(MyGraph g, int startingVertex) {
+        //Set all vertex distances to the max value (∞)
+        int n = g.vertices.size();
+        int[] dist = new int[n];
+        //Set all previous to -1
+        int[] previous = new int[n];
+        //Set all visited to false
+        boolean[] visited = new boolean[n];
+        //Put all vertices in unvisitedList
+        List<Integer> unvisitedList = new ArrayList<>(g.vertices);
+
+        for (int v : g.vertices) {
+            dist[v] = Integer.MAX_VALUE;
+            previous[v] = -1;
+            visited[v] = false;
+        }
+
+        //Pick a starting vertex and set its distance to 0
+        dist[startingVertex] = 0;
+        //While unvisitedList not empty
+        while (!unvisitedList.isEmpty()) {
+            //currV = GetMinDistVertex(unvisitedList, dist)
+            int currV = getMinDistVertex(g, unvisitedList, dist);
+            //Remove currV from unvisitedList and set visited[currV] to true
+            unvisitedList.remove(Integer.valueOf(currV));
+            visited[currV] = true;
+            //For all unvisited neighbors of currV (call them n)
+            for (Edge e : g.adjacencyList.get(currV)) {
+                int nVertex = (e.v1 == currV ? e.v2 : e.v1);
+                //Set possibleDist to (distance[currV] + weight of edge to n)
+                int possibleDist = dist[nVertex] + e.weight;
+                //if possibleDist < dist[n]
+                if (possibleDist < dist[nVertex]) {
+                    //Set dist[n] to possibleDist
+                    dist[nVertex] = possibleDist;
+                    //Set previous[n] to currV
+                    previous[nVertex] = currV;
+                }
+            }
+        }
+        for (int i : g.vertices) {
+            System.out.println("Vertex " + i + ", dist " + dist[i] + ", previous " + previous[i]);
+        }
+
+    }
+
+    }
+
 
